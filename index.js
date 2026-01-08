8aba55dce873f95ee670fe5f2d908afd0bb79cfa
nishith <nishith.one@gmail.com>
2026-01-08 21:19:29 +0530

    fix: added bug fix

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ const startServer = async () => {
       await mongoose.connect(process.env.DB_URL);
     }
 
-    // Sync indexes safely (dev / test only)
-    if (process.env.NODE_ENV !== "production") {
-      await Theatre.syncIndexes();
-    }
-
     // Connect to Production DB
     if (process.env.NODE_ENV === "production") {
       await mongoose.connect(process.env.PROD_DB_URL);
